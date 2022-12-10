2ed5f47048a6ad9f2fe37b737b6d1b76ceea84ce
lztforgithub <101919058+lztforgithub@users.noreply.github.com>
2022-12-10 22:47:25 +0800

    古怪的跑起来了

--- src/main/java/ES/config/MvcConfig.java
+++ src/main/java/ES/config/MvcConfig.java
@@ -25,7 +25,6 @@ public class MvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/user/**")
                 .excludePathPatterns("/comment/**")
                 .excludePathPatterns("/author/**");
-
     }
 
     /**
