c87688ab19afd2768f8ccaffc740f7ee41df6027
lztforgithub <101919058+lztforgithub@users.noreply.github.com>
2022-12-10 15:39:11 +0800

    Bug修复，出版物信息查询

--- src/main/java/ES/Service/ServiceImpl/VenueServiceImpl.java
+++ src/main/java/ES/Service/ServiceImpl/VenueServiceImpl.java
@@ -8,6 +8,11 @@ import org.elasticsearch.common.recycler.Recycler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class VenueServiceImpl implements VenueService {
 
@@ -20,6 +25,16 @@ public class VenueServiceImpl implements VenueService {
         if (jsonObject==null){
             return Response.fail("VID错误!");
         }
+        List<String> CID = new ArrayList<>();
+        List<String> Cname = new ArrayList<>();
+        CID = (List<String>) jsonObject.get("VconceptIDs");
+
+        for (String i: CID){
+            JSONObject t = esUtileService.queryDocById("concept",i);
+            Cname.add(t.getString("Cname"));
+        }
+
+        jsonObject.put("Cname",Cname);
         return Response.success("出版物信息如下:",
                 jsonObject);
     }
