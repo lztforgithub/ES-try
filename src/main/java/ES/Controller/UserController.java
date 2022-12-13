d0a20c56a844cebf1cc7bf76250c86082f6a2357
Will Frog <froggywch@gmail.com>
2022-12-13 17:23:18 +0800

    findPassword bug fix

--- src/main/java/ES/Controller/UserController.java
+++ src/main/java/ES/Controller/UserController.java
@@ -102,7 +102,6 @@ public class UserController {
     public Response<Object> confirmVerCode(HttpServletRequest request,@RequestBody Map<String,String> map){
         String email = map.get("email");
         String verCode = map.get("vercode");
-        System.out.printf("Get email and vercode %s, %s\n", email, verCode);
         return userService.confiemVerCode(email,verCode);
     }
 
